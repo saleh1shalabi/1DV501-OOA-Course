1deb27da7d38805f5f26eb38d67653a9f5a8cd23
Saleh Shalabi <ss225bx@student.lnu.se>
2021-03-16 20:33:20 +0100

    .

--- app/src/test/java/RecipApp/AppTest.java
+++ app/src/test/java/RecipApp/AppTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test
-    public void testAppHasAGreeting() {
-        Ingredient classUnderTest = new Ingredient("null", "null", 0);
+    public void testAppCreatesIngredient() {
+        Ingredient classUnderTest = new Ingredient(null, "null", 0);
         assertNotNull("app gets name of  Ingredients", classUnderTest.getName());
 
     }
